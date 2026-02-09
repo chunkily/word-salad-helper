15c427df19795b197bc39846dc43817d9ef95977
Lee Chengkai <leechengkai@gmail.com>
2026-02-09 16:20:41 +0800

    Initialize repository

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+</script>
+
+<div>
+	<ul>
+		{#each data.wordlist as word}
+			<li>{word}</li>
+		{/each}
+	</ul>
+</div>
